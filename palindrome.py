aca772f32be8193a57dac92ecdb2e8266aab9082
AayushQ <aayush.agrawal@quantiphi.com>
2017-07-10 17:10:31 +0530

    first commit

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+str1 = input("Enter a string ")
+str2 = str1[::-1]
+if str1==str2:
+	print "Is Palindrome"
+else:
+	print "Not a palindrome"
+ 
---
+++ string1.py
@@ -0,0 +1,3 @@
+name = input("What's your name?")
+print name
+print("Its great to meet you " + name + "!")
